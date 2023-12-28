e35f4c682507306a9597cbf9253893d6e7244b86
Linas Stonkus <liniusst@gmail.com>
2023-12-28 09:21:00 +0200

    update

--- app.py
+++ app.py
@@ -1,99 +1,75 @@
-import cv2
+import cv2 as cv
 from glob import glob
 from ultralytics import YOLO
 import utils
 import torch
 import numpy as np
+from utils import write_csv, read_license_plate
 
 # pick pre-trained model
 np_model = YOLO("best.pt")
+coco_model = YOLO('yolov8n.pt')
+
+# read in test video paths
+videos = glob('inputs/*.mp4')
+print(videos)
 
 results = {}
 
 # read video by index
-videos = glob("inputs/*.mp4")
-video = cv2.VideoCapture(videos[0])
+video = cv.VideoCapture(videos[0])
 
 ret = True
 frame_number = -1
 vehicles = [2, 3, 5, 7]
-license_plate_detected = False
 
-allow_list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-
-def process_detection(image, box):
-    x1, y1, x2, y2, _, _ = box
-    region_of_interest = image[int(y1) : int(y2) + 5, int(x1) : int(x2) + 5]
-    gray_image = cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2GRAY)
-    bfilter = cv2.bilateralFilter(gray_image, 11, 17, 17)
-    _, thresholded = cv2.threshold(bfilter, 64, 255, cv2.THRESH_BINARY_INV)
-    return thresholded
-
-
-# read the 10 first frames
+# read every 2 frames
 while ret:
     frame_number += 1
     ret, frame = video.read()
 
-    if ret and frame_number < 50:
-        # results[frame_number] = {}
+    if ret and frame_number % 2 == 0:  # Process every 2 frames
+        results[frame_number] = {}
 
-        # license plate detector for the whole frame
-        with torch.no_grad():
-            license_plate = np_model(frame)[0]
+        # vehicle detector
+        detections = coco_model.track(frame, persist=True)[0]
+        for detection in detections.boxes.data.tolist():
+            x1, y1, x2, y2, track_id, score, class_id = detection
+            if int(class_id) in vehicles and score > 0.5:
+                vehicle_bounding_boxes = []
+                vehicle_bounding_boxes.append([x1, y1, x2, y2, track_id, score])
+                for bbox in vehicle_bounding_boxes:
+                    print(bbox)
+                    roi = frame[int(y1):int(y2), int(x1):int(x2)]
 
-        img_tresh = [
-            process_detection(frame, box) for box in license_plate.boxes.data.tolist()
-        ]
-        for img in img_tresh:
-            cnts, _ = cv2.findContours(
-                np.vstack(img).astype(np.uint8),
-                cv2.RETR_EXTERNAL,
-                cv2.CHAIN_APPROX_SIMPLE,
-            )
-            cnt = max(cnts, key=cv2.contourArea)
-            x, y, w, h = cv2.boundingRect(cnt)
+                    # license plate detector for region of interest
+                    license_plates = np_model(roi)[0]
+                    # process license plate
+                    for license_plate in license_plates.boxes.data.tolist():
+                        plate_x1, plate_y1, plate_x2, plate_y2, plate_score, _ = license_plate
+                        # crop plate from region of interest
+                        plate = roi[int(plate_y1):int(plate_y2), int(plate_x1):int(plate_x2)]
+                        # de-colorize
+                        plate_gray = cv.cvtColor(plate, cv.COLOR_BGR2GRAY)
+                        # posterize
+                        _, plate_treshold = cv.threshold(plate_gray, 64, 255, cv.THRESH_BINARY_INV)
 
-            results = utils.reader.readtext(
-                np.vstack(img), allowlist=allow_list, paragraph=False
-            )
+                        # OCR
+                        np_text, np_score = read_license_plate(plate_treshold)
+                        # if plate could be read write results
+                        if np_text is not None:
+                            results[frame_number][track_id] = {
+                                'car': {
+                                    'bbox': [x1, y1, x2, y2],
+                                    'bbox_score': score
+                                },
+                                'license_plate': {
+                                    'bbox': [plate_x1, plate_y1, plate_x2, plate_y2],
+                                    'bbox_score': plate_score,
+                                    'number': np_text,
+                                    'text_score': np_score
+                                }
+                            }
 
-        # process license plates
-        # for license_plate in license_plates.boxes.data.tolist():
-        #     (
-        #         plate_x1,
-        #         plate_y1,
-        #         plate_x2,
-        #         plate_y2,
-        #         plate_score,
-        #         _,
-        #     ) = license_plate
-
-        # crop plate from the frame
-        # plate = frame[int(plate_y1) : int(plate_y2), int(plate_x1) : int(plate_x2)]
-
-        # de-colorize
-        # plate_gray = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
-
-        # posterize
-        # _, plate_threshold = cv2.threshold(plate_gray, 64, 255, cv2.THRESH_BINARY_INV)
-
-        # OCR
-        # np_text, np_score = utils.read_license_plate(plate_threshold)
-
-        # if plate could be read, write results
-        # if np_text is not None:
-        #     results[frame_number][1] = {
-        #         "license_plate": {
-        #             "bbox": [plate_x1, plate_y1, plate_x2, plate_y2],
-        #             "bbox_score": plate_score,
-        #             "number": np_text,
-        #             "text_score": np_score,
-        #         },
-        #     }
-
-# write results to CSV
-# utils.write_csv(results, "./results.csv")
-print(results)
+write_csv(results, './results.csv')
 video.release()
--- main.py
+++ main.py
@@ -1,51 +1,128 @@
-import ast
-import cv2 as cv
-import easyocr
-from glob import glob
+import cv2
 import numpy as np
-import pandas as pd
-import string
+import easyocr
+import re, os
+import torch
 from ultralytics import YOLO
 
-# regular pre-trained yolov8 model for car recognition
-# coco_model = YOLO('yolov8n.pt')
-coco_model = YOLO("yolov8s.pt")
-# yolov8 model trained to detect number plates
-np_model = YOLO("best.pt")
+torch.device("cpu")
+model_path = os.path.join("best.pt")
+model = YOLO(model_path)
 
-# read in test video paths
-videos = glob("inputs/*.mp4")
+class detect_license_plate:
+    def __init__(self, video_path=0) -> None:
+        self.video_path = video_path
+        self.result = None
 
-# read video by index
-video = cv.VideoCapture(videos[0])
+    def _read_frame(self):
+        try:
+            cap = cv2.VideoCapture(self.video_path)
+            ret, frame = cap.read()
+            cap.release()
+            if not ret:
+                print("Error reading frame.")
+                return None
+            return frame
+        except Exception as e:
+            print(f"Error during frame reading: {e}")
+            return None
 
-ret = True
-frame_number = -1
-# all vehicle class IDs from the COCO dataset (car, motorbike, truck) https://docs.ultralytics.com/datasets/detect/coco/#dataset-yaml
-vehicles = [2, 3, 5]
-vehicle_bounding_boxes = []
+    def _frame_to_thresh(self, frame):
+        if frame is None:
+            self.frame_thresh = []
+            return
 
-# read the 10 first frames
-while ret:
-    frame_number += 1
-    ret, frame = video.read()
+        try:
+            detections = model(frame)[0]
+            self.frame_thresh = [
+                cv2.threshold(
+                    cv2.cvtColor(
+                        frame[int(y1) : int(y2), int(x1) : int(x2)], cv2.COLOR_BGR2GRAY
+                    ),
+                    64,
+                    255,
+                    cv2.THRESH_BINARY_INV,
+                )[1]
+                for x1, y1, x2, y2, _, _ in detections.boxes.data.tolist()
+            ]
+        except Exception as e:
+            print(f"Error during frame processing: {e}")
+            self.frame_thresh = []
 
-    if ret and frame_number < 10:
-        # use track() to identify instances and track them frame by frame
-        detections = coco_model.track(frame, persist=True)[0]
-        # save cropped detections
-        # detections.save_crop('outputs')
-        # print nodel predictions for debugging
-        # print(results)
+    def frame_cnts(self):
+        if not hasattr(self, "frame_thresh") or not self.frame_thresh:
+            return []
 
-        for detection in detections.boxes.data.tolist():
-            # print detection bounding boxes for debugging
-            # print(detection)
-            x1, y1, x2, y2, track_id, score, class_id = detection
-            # I am only interested in class IDs that belong to vehicles
-            if int(class_id) in vehicles and score > 0.5:
-                vehicle_bounding_boxes.append([x1, y1, x2, y2, track_id, score])
+        try:
+            reader = easyocr.Reader(["en"], gpu=False, verbose=False)
 
-# print found bounding boxes for debugging
-print(vehicle_bounding_boxes)
-video.release()
+            common_width = 100
+
+            resized_frames = [
+                cv2.resize(frame, (common_width, frame.shape[0]))
+                for frame in self.frame_thresh
+            ]
+
+            cnts, _ = cv2.findContours(
+                np.vstack(resized_frames).astype(np.uint8),
+                cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE,
+            )
+
+            if not cnts:
+                return []
+
+            cnt = max(cnts, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(cnt)
+            output = reader.readtext(np.vstack(self.frame_thresh), paragraph=False)
+        except Exception as e:
+            print(f"Error during contour extraction or OCR: {e}")
+            output = []
+
+        return output
+
+    def process_video(self):
+        try:
+            cap = cv2.VideoCapture(self.video_path)
+            frame_counter = 0
+
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                frame_counter += 1
+
+                # Check every 5 frames
+                if frame_counter % 2 == 0:
+                    self._frame_to_thresh(frame)
+                    output = self.frame_cnts()
+                    license_plate_list = [
+                        re.sub(r"[^A-Z0-9]", "", text)
+                        for _, text, text_score in output
+                        if text_score > 0.15
+                    ]
+
+                    license_plate = "".join(license_plate_list)
+                    self.result = {"plate": license_plate}
+                    print(self.result)  # You can replace this with your desired processing
+
+                # Break the loop if 'q' key is pressed
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            cap.release()
+            cv2.destroyAllWindows()
+
+        except Exception as e:
+            print(f"Error during video processing: {e}")
+
+
+# Usage for video from file
+try:
+    video_path = "apkarpytas.mp4"
+    detection = detect_license_plate(video_path)
+    detection.process_video()
+except Exception as e:
+    print(f"Error during license plate detection: {e}")
+    # Handle the error or exit the program
